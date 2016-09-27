2d6bd5ae271de1875873e2f1671ea40cd0d4bb12
clyde80 <zrobbert0998@gmail.com>
2016-09-27 15:30:04 -0500

    Update install.sh
    Removed to useless lines.

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-git clone https://github.com/clyde80/i3-configuration
-cd i3-configuration
 cp .Xresources ~/
 cp config ~/.config/i3
 cp firewatch-orange-purple.theme /usr/share/xfce4/terminal/colorschemes
