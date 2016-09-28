7825298a2cc4b0532938caf850f385a20c5b6dce
clyde80 <zrobbert0998@gmail.com>
2016-09-28 17:46:40 -0500

    Changed all files to fit new theme needs

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 cp .Xresources ~/
 cp config ~/.config/i3
-cp firewatch-orange-purple.theme /usr/share/xfce4/terminal/colorschemes
+cp terminal_themes/firewatch-orange-purple.theme /usr/share/xfce4/terminal/colorschemes
+cp terminal_themes/edinburgh-hotel.theme /usr/share/xfce4/terminal/colorschemes
 cp i3status.conf /etc
 cp terminalrc ~/.config/xfce4/terminal
+cp autosave ~/.config/cmus
