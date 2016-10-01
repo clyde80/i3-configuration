8c2c0c495db2ec9daa65674cfeb00086df569c41
clyde80 <2j9clkxj09lfjer23o@tutanota.com>
2016-10-01 16:46:26 -0500

    Added a fancy Rofi setup script
    runrofi.sh now takes SS of screen, blurs it, and then runs rofi with that as the background. Rofi is now full screen. install.sh was changed to copy the runrofi.sh file to /usr/bin. config was changed so when +L was pressed, runrofi.sh would run instead of the other command used. Running rofi this way takes more time though, so remove the scrot & convert commands and remove the -fake-background option from the rofi command if you don't want it to take the screenshot and all that fancy stuff.

--- install.sh
+++ install.sh
@@ -6,3 +6,4 @@ cp terminal_themes/edinburgh-hotel.theme /usr/share/xfce4/terminal/colorschemes
 cp i3status.conf /etc
 cp terminalrc ~/.config/xfce4/terminal
 cp autosave ~/.config/cmus
+cp runrofi.sh /usr/bin
