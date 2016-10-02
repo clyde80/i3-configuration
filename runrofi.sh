17efdd7ddb3bc5a0447a582d4d1f3e2e830bf024
clyde80 <2j9clkxj09lfjer23o@tutanota.com>
2016-10-02 12:47:51 -0500

    Added comment to runrofi.sh

--- runrofi.sh
+++ runrofi.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# Takes a screenshot of the desktop, blurs it, and then runs rofi with fake transparency and the blurred image as the background, in full screen mode.
 scrot /tmp/screenshot.png && convert -blur 5x5 /tmp/screenshot.png /tmp/screenshot.png && rofi -show run -hide-scrollbar -fullscreen -padding 200 -separator-style none -fake-transparency true -fake-background /tmp/screenshot.png
